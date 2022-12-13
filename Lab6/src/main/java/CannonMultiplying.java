b32cad67f2ffd69e8e4af68b5752b2c5ad16e1ee
olyzko <84335033+olyzko@users.noreply.github.com>
2022-12-13 23:29:57 +0200

    Lab6 done

---
+++ Lab6/src/main/java/CannonMultiplying.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class CannonMultiplying {
+    private static int[][] a;
+    private static int[][] b;
+    private static int process_amount;
+    private static int[][] res;
+
+    public static int[][] multiply(int[][] a, int[][] b, int process_amount){
+        CannonMultiplying.a = a;
+        CannonMultiplying.b = b;
+        CannonMultiplying.process_amount = process_amount;
+
+        res = new int[a.length][a.length];
+
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a.length; j++){
+                res[i][j] = 0;
+            }
+        }
+
+        Thread[] tasks = new Thread[process_amount];
+        for(int i = 0; i < tasks.length; i++){
+            tasks[i] = new Thread(new Task(i));
+        }
+
+        for (Thread task : tasks) {
+            task.start();
+
+        }
+
+        for (Thread task : tasks) {
+            try {
+                task.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return res;
+    }
+
+    private record Task(int part_index) implements Runnable {
+
+        @Override
+        public void run() {
+            int pivot = (int) Math.ceil(a.length / (double) process_amount);
+            for (int row = part_index * pivot; row < (part_index + 1) * pivot && row < a.length; row++) {
+                int counter = 0;
+                int a_j = row;
+                int b_i = row;
+                while (counter < a.length) {
+                    for (int i = 0; i < a.length; i++) {
+                        res[row][i] += a[row][a_j] * b[b_i][i];
+                    }
+
+                    a_j = (a.length + a_j - 1) % a.length;
+                    b_i = (a.length + b_i - 1) % a.length;
+                    counter++;
+                }
+            }
+        }
+    }
+}
---
+++ Lab6/src/main/java/TapeMultiplying.java
@@ -0,0 +1,66 @@
+public class TapeMultiplying {
+
+    private static int[][] a;
+    private static int[][] b;
+    private static int process_amount;
+    private static int[][] res;
+
+    public static int[][] multiply(int[][] a, int[][] b, int process_amount){
+        TapeMultiplying.a = a;
+        TapeMultiplying.b = b;
+        TapeMultiplying.process_amount = process_amount;
+
+        res = new int[a.length][a.length];
+
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a.length; j++){
+                res[i][j] = 0;
+            }
+        }
+
+        Thread[] tapes = new Thread[process_amount];
+        for(int i = 0; i < tapes.length; i++){
+            tapes[i] = new Thread(new Tape(i));
+        }
+
+        for (Thread tape : tapes) {
+            tape.start();
+
+        }
+
+        for (Thread tape : tapes) {
+            try {
+                tape.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return res;
+    }
+
+    private record Tape(int part_index) implements Runnable {
+
+        @Override
+        public void run() {
+
+            int pivot = (int) Math.ceil(a.length / (double) process_amount);
+            for (int row = part_index * pivot; row < (part_index + 1) * pivot && row < a.length; row++) {
+                int counter = 0;
+                int index = row;
+                while (counter < a.length) {
+                    int cell = 0;
+                    for (int i = 0; i < a.length; i++) {
+                        cell += a[row][i] * b[i][index];
+                    }
+
+                    res[row][index] = cell;
+                    counter++;
+                    index = (index + 1) % a.length;
+                }
+            }
+        }
+    }
+
+
+}
